3c23a25ccd676f8766165b72fec08991df09f17f
baoacer <nguyenquocbaotu@gmail.com>
2025-02-19 07:36:16 +0700

    update comment

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,68 +1,159 @@
-const Comment = require('../models/comment.model');
-const Post = require('../models/post.model');
-const User = require('../models/user.model');
+const { BadRequestError } = require("../core/error.response");
+const Comment = require("../models/comment.model");
+const Post = require("../models/post.model");
+const User = require("../models/user.model");
 
-class CommentService{
-    static async createComment({
-        postId, userId, content, commentParentId, 
-    }){
-        // check postid exist
-        const foundPost = await Post.findOne({ _id: postId });
-        if(!foundPost) throw new Error('Post not found');
+class CommentService {
+  static async createComment({ postId, userId, content, commentParentId }) {
+    // check postid exist
+    const foundPost = await Post.findOne({ _id: postId });
+    if (!foundPost) throw new Error("Post not found");
 
-        // check userId exist
-        const foundUser = await User.findOne({ _id: userId });
-        if(!foundUser) throw new Error('User not found');
+    // check userId exist
+    const foundUser = await User.findOne({ _id: userId });
+    if (!foundUser) throw new Error("User not found");
 
-        if(content === '' || content.length == 0) throw new Error('Content is empty');
+    if (content === "" || content.length == 0)
+      throw new Error("Content is empty");
 
-        // create Comment
-        const comment = await Comment.create({
-            comment_post_id: postId,
-            comment_user_id: userId,
-            comment_content: content,
-            comment_parent_id: commentParentId
-        })
+    // create Comment
+    const comment = await Comment.create({
+      comment_post_id: postId,
+      comment_user_id: userId,
+      comment_content: content,
+      comment_parent_id: commentParentId,
+    });
 
-        let rightValue
-        if(commentParentId){
-            const commentParent = await Comment.findOne({ _id: commentParentId })
-            if(!commentParent) throw new Error('Comment parent not found')
+    let rightValue;
+    if (commentParentId) {
+      const commentParent = await Comment.findOne({ _id: commentParentId });
+      if (!commentParent) throw new Error("Comment parent not found");
 
-            rightValue = commentParent.comment_right
+      rightValue = commentParent.comment_right;
 
-            await Comment.updateMany({
-                comment_post_id: postId,
-                comment_right: { $gte: rightValue }
-            }, {
-                $inc: { comment_right: 2 }
-            })
-
-            await Comment.updateMany({
-                comment_post_id: postId,
-                comment_left: { $gt: rightValue }
-            }, {
-                $inc: { comment_left: 2 }
-            })
-        }else{
-            const maxRightValue = await Comment.findOne({
-                comment_post_id: postId
-            }, 'comment_right')
-            .sort({ comment_right: -1 })
-            
-            if(maxRightValue){
-                rightValue = maxRightValue ? maxRightValue.comment_right + 1 : 1;
-            }else{
-                rightValue = 1
-            }
+      await Comment.updateMany(
+        {
+          comment_post_id: postId,
+          comment_right: { $gte: rightValue },
+        },
+        {
+          $inc: { comment_right: 2 },
         }
+      );
 
-        comment.comment_left = rightValue
-        comment.comment_right = rightValue + 1
-        await comment.save()
+      await Comment.updateMany(
+        {
+          comment_post_id: postId,
+          comment_left: { $gt: rightValue },
+        },
+        {
+          $inc: { comment_left: 2 },
+        }
+      );
+    } else {
+      const maxRightValue = await Comment.findOne(
+        {
+          comment_post_id: postId,
+        },
+        "comment_right"
+      ).sort({ comment_right: -1 });
+
+      if (maxRightValue) {
+        rightValue = maxRightValue ? maxRightValue.comment_right + 1 : 1;
+      } else {
+        rightValue = 1;
+      }
+    }
+
+    comment.comment_left = rightValue;
+    comment.comment_right = rightValue + 1;
+    await comment.save();
+
+    return comment;
+  }
+
+  static async deleteComment({ postId, commentId }) {
+    const foundPost = await Post.findOne({ _id: postId });
+    if (!foundPost) throw new BadRequestError("Post not found");
+
+    const foundComment = await Comment.findOne({ _id: commentId });
+    if (!foundComment) throw new BadRequestError("Comment not found");
+
+    const { comment_left, comment_right } = foundComment;
+
+    const width = comment_right - comment_left + 1;
+
+    await Comment.deleteMany({
+      comment_post_id: postId,
+      comment_left: { $gte: comment_left },
+      comment_right: { $lte: comment_right },
+    });
+
+    await Comment.updateMany(
+      {
+        comment_post_id: postId,
+        comment_right: { $gt: comment_right },
+      },
+      {
+        $inc: { comment_right: -width },
+      }
+    );
+
+    await Comment.updateMany(
+      {
+        comment_post_id: postId,
+        comment_left: { $gt: comment_right },
+      },
+      {
+        $inc: { comment_left: -width },
+      }
+    );
+  }
+  
+  static async getCommentByParentId({
+    postId,
+    commentParentId = null,
+    limit = 50,
+    skip = 0,
+  }) {
+    if(commentParentId){
+        const parent = await Comment.findOne({ _id: commentParentId })
+        if(!parent) throw new BadRequestError('Comment not found')
+
+        const comment = await Comment.find({
+            comment_post_id: postId,
+            comment_left: { $gt: parent.comment_left },
+            comment_right: { $lte: parent.comment_right }
+        })
+        .select({
+          comment_left: 1,
+          comment_right: 1,
+          comment_content: 1,
+          comment_parent_id: 1
+        })
+        .sort({ comment_left: 1})
+        .limit(limit)
+        .skip(skip)
 
         return comment
+    }else{
+      const comment = await Comment.find({
+        comment_post_id: postId,
+        comment_parent_id: commentParentId
+      })
+      .select({
+        comment_left: 1,
+        comment_right: 1,
+        comment_content: 1,
+        comment_parent_id: 1
+      })
+      .sort({ comment_left: 1})
+      .limit(limit)
+      .skip(skip)
+
+      return comment
     }
+  }
 }
 
 module.exports = CommentService;
