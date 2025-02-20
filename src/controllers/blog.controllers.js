de7ec1f2b15dfecd5de031de06a618868b23a24a
ngochoang922 <ngochoang9022@gmail.com>
2025-02-20 16:03:01 +0700

    update Crud

--- src/controllers/blog.controllers.js
+++ src/controllers/blog.controllers.js
@@ -23,10 +23,54 @@ class BlogController {
     }
 
     // Lay post bang id
+    static async getPostById(req, res) {
+        try {
+            const { id } = req.params;
+            const result = await BlogService.getPostById(id);
+            return res.status(200).json(result);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
 
     // xoa post = id
- 
+    static async deletePost(req, res) {
+        try {
+            const { id } = req.params;
+            const result = await BlogService.deletePost(id);
+            return res.status(200).json(result);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
     // update post bang id
+    static async updatePost(req, res) {
+        try {
+            const { id } = req.params;
+            const payload = req.body;
+            const result = await BlogService.updatePost(id, req.body);
+            return res.status(200).json(result);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+    static async findPostByTitle(req, res) {
+        try {
+            console.log("Dữ liệu nhận từ body:", req.body); // Kiểm tra dữ liệu body gửi lên
+            const { title } = req.body;
+    
+            if (!title) {
+                return res.status(400).json({ error: "Thiếu tiêu đề bài viết" });
+            }
+    
+            const result = await BlogService.findPostByTitle(title);
+            return res.status(200).json(result);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+    
+
 }
 
 module.exports = BlogController;
--- src/routers/blog/index.js
+++ src/routers/blog/index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 const BlogController = require('../../controllers/blog.controllers')
 
-router.get('', blogController.getPosts);
+router.get('', BlogController.getPosts);
 router.post('', BlogController.createPost)
+router.get('/:id', BlogController.getPostById)
+router.delete('/:id', BlogController.deletePost)
+router.put('/:id', BlogController.updatePost)
+router.post('/find', BlogController.findPostByTitle);
 
 module.exports = router
+
