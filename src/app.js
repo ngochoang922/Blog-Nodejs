fd9dfd5af6b9ff4e2ea672075de9b174b8d93fc5
baoacer <nguyenquocbaotu@gmail.com>
2025-02-27 14:16:26 +0700

    fix view

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const router = require('./routers/index')
+const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
