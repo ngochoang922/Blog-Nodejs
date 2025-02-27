3399c2dcbdf8c74276dd8fb85b49117bcea5bf26
ngochoang922 <ngochoang9022@gmail.com>
2025-02-27 14:01:07 +0700

    new View

--- src/app.js
+++ src/app.js
@@ -11,7 +11,21 @@ require('./database/init.mongodb')
 // init router
 app.use('/', router)
 
+// Serve static files từ thư mục public
+app.use(express.static('public'));
 
+// Route để trả về file HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
 
 
 module.exports = app
