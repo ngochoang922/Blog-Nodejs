b975f7c527b396f53ac4b057600d762f899a58e2
baoacer <nguyenquocbaotu@gmail.com>
2025-02-27 21:52:58 +0700

    update

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const router = require('./routers/index')
 const path = require('path')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use(cors({
+    origin: '*'
+}))
 
 // init database
 require('./database/init.mongodb')
